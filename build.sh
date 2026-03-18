be8b930bd3d732ba7fdf2b3bb8ddcff03e418273
TrikiCode <tristan.flander30@gmail.com>
2026-03-18 11:56:42 +0100

    added build line

--- build.sh
+++ build.sh
@@ -1,3 +1,4 @@
 colcon build --packages-select msg_srv_pkg 
 colcon build --packages-select dis_tutorial3 --symlink-install
 colcon build --packages-select dis_tutorial4 --symlink-install
+colcon build --packages-select task1 --symlink-install
